98aa4a5ae5d32bdd35cd67fcbbec3509e8fffe93
Diego Guzman <diegp2001@gmail.com>
2025-06-15 17:40:24 -0600

    Initial Commit

---
+++ ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Diego Guzman on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactAppTests/ContactAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppTests.swift
+//  ContactAppTests
+//
+//  Created by Diego Guzman on 15/06/25.
+//
+
+import Testing
+@testable import ContactApp
+
+struct ContactAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
